0d96fd97ef41df62795210baa92ca590b6341edb
evilb1ue <394250583@qq.com>
2018-09-01 12:38:22 +0800

    Initial commit

---
+++ Irise/__init__.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+
+import pymysql#Use this replace MySQLdb(There has problem between MySQLdb and python3.6 )
+pymysql.install_as_MySQLdb()
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/root/Desktop/初升/Irise/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
